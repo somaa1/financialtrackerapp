4d3d33d700415d8501b8963feaacafa2903f665d
ismailrabie <ismailrabie111@gmail.com>
2024-12-12 11:03:39 +0200

    some edit in home view

--- lib/views/home/widget/home_view_body.dart
+++ lib/views/home/widget/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:financialtrackerapp/data/list_data.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -5,45 +6,268 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return CustomScrollView(slivers: [
+      SliverToBoxAdapter(
+        child: SizedBox(height: 350, child: headWidget()),
+      ),
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Translations history",
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 19,
+                    color: Colors.black),
+              ),
+              Text(
+                "See all",
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15,
+                    color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+      ),
+      SliverList(delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Image.asset('assets/images/${getter()[index].image!}'),
+            ),
+            title: Text(
+              getter()[index].name!,
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 17,
+              ),
+            ),
+            subtitle: Text(
+              getter()[index].time!,
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            trailing: Text(
+            "\$ ${  getter()[index].fee!}",
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 19,
+                  color: Colors.red),
+            ),
+          );
+
+
+        },
+        childCount: getter().length,
+      ))
+    ]);
+  }
+}
+
+class headWidget extends StatelessWidget {
+  const headWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          height: 240,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-              color: Color(0xFF368983),
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20))),
-          child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 35,left: 10),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(7),
-                  child: Container(
-                    height: 40,
-                    width: 40,
-                    color: const Color.fromRGBO(250, 250, 250, 0.1),
-                    child: const Icon(
-                      Icons.notification_add,
-                      color: Colors.white,
-                      size: 30,
+        Column(
+          children: [
+            Container(
+              height: 240,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                  color: Color(0xFF368983),
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20))),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 350,
+                    top: 35,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(7),
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        color: const Color.fromRGBO(250, 250, 250, 0.1),
+                        child: const Icon(
+                          Icons.notification_add_outlined,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 35, left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good Morning",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20,
+                              color: Color.fromARGB(255, 224, 223, 223)),
+                        ),
+                        Text(
+                          "Ismail Rabie",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-               Positioned(
-                 left: 300,
-                 top: 35,
-                 child: Column(
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                   children: [
-                  Text("صباح الخير",style: TextStyle(fontWeight:FontWeight.w500, fontSize: 20,color: Color.fromARGB(255, 224, 223, 223)),),
-                  Text("أسماعيل ربيع",style: TextStyle(fontWeight:FontWeight.w600, fontSize: 20,color: Colors.white),),
-                               ],),
-               )
-            ],
+            ),
+          ],
+        ),
+        Positioned(
+          top: 140,
+          left: 37,
+          child: Container(
+            height: 170,
+            width: 320,
+            decoration: BoxDecoration(
+                color: Color.fromRGBO(47, 124, 120, 1),
+                borderRadius: BorderRadius.circular(15)),
+            child: const Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total Amount',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                            color: Color.fromARGB(255, 224, 223, 223)),
+                      ),
+                      Icon(
+                        Icons.more_horiz,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 7,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Row(
+                    children: [
+                      Text('\$ 2,239.00',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                              color: Colors.white))
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 13,
+                            backgroundColor: Color.fromARGB(255, 85, 145, 141),
+                            child: Icon(
+                              Icons.arrow_downward,
+                              color: Colors.white,
+                              size: 19,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 7,
+                          ),
+                          Text(
+                            "income",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 13,
+                            backgroundColor: Color.fromARGB(255, 85, 145, 141),
+                            child: Icon(
+                              Icons.arrow_upward,
+                              color: Colors.white,
+                              size: 19,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 7,
+                          ),
+                          Text(
+                            "Expense",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "\$ 2.999",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        "\$ 1.99",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         )
       ],
